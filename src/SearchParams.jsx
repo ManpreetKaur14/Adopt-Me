f85d4dab1738429acf02ced7642ce00a64d4d028
Manpreet Kaur <manpreetkaur0699@gmail.com>
2024-04-07 23:31:10 +0530

    React First Commit

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"]
 const SearchParams = () => {
     const [location, setLocation] = useState(""); 
-    return(
+    const [animal, setAnimal] = useState("");
+    const [breed, setBreed] = useState("");
+        return(
         <div className="search-params">
             <form>
                 <label htmlFor="location">
@@ -9,6 +13,38 @@ const SearchParams = () => {
                     <input
                     onChange={(e) => setLocation(e.target.value)}
                      id="location" value={location} placeholder="Location" />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                    id="animal"
+                    value={animal}
+                    
+                    onChange={(e) =>
+                    {
+                        setAnimal(e.target.value);
+                    }}>
+                        {ANIMALS.map((animal)=>
+                        (
+                            <option key={animal}>{animal}</option>
+                        ))}
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    breed
+                    <select
+                    id="breed"
+                    value={breed}
+                    
+                    onChange={(e) =>
+                    {
+                        setBreed(e.target.value);
+                    }}>
+                        {ANIMALS.map((animal)=>
+                        (
+                            <option key={animal}>{animal}</option>
+                        ))}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
